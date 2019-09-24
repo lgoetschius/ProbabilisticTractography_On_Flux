7814d67f1058c2663ba56b48940aa7c425305b82
Leigh Goetschius <lcgayle@umich.edu>
2019-09-24 13:01:34 -0400

    UPloading slspec

---
+++ slspec_maker.m
@@ -0,0 +1,13 @@
+fp = fopen('dicom_DTI_64_20141222100138_8a.json','r');
+fcont = fread(fp);
+fclose(fp);
+cfcont = char(fcont');
+i1 = strfind(cfcont,'SliceTiming');
+i2 = strfind(cfcont(i1:end),'[');
+i3 = strfind(cfcont((i1+i2):end),']');
+cslicetimes = cfcont((i1+i2+1):(i1+i2+i3-2));
+slicetimes = textscan(cslicetimes,'%f','Delimiter',',');
+[sortedslicetimes,sindx] = sort(slicetimes{1});
+mb = length(sortedslicetimes)/(sum(diff(sortedslicetimes)~=0)+1);
+slspec = reshape(sindx,[mb length(sindx)/mb])'-1;
+dlmwrite('slspec.txt',slspec,'delimiter',' ','precision','%3d');
